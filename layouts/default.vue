4ed13432e5bf1998e4a9d54332e4aac51ce51c53
user <igor.goshkiev@yandex.ru>
2025-03-23 17:14:01 +0300

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+      <div class="header">
+          <MainMenu />
+      </div>
+      <div class="content">
+          <slot />
+      </div>
+  </div>
+</template>
+<style scoped>
+.header {
+  padding: 15px;
+  border-bottom: 1px solid #EEE;
+}
+.content {
+  padding: 15px;
+}
+</style>
